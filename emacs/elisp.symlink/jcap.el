1f4d17a638789f04e30c68fc35579b341067aa99
Victor Santos (vct) <victor.phb@gmail.com>
2015-07-30 18:29:08 -0300

    Added custom elisp script folder

---
+++ emacs/elisp.symlink/jcap.el
@@ -0,0 +1,14 @@
+(require 'ox-latex)
+(unless (boundp 'org-latex-classes)
+  (setq org-latex-classes nil))
+(add-to-list 'org-latex-classes
+             '("jcap"
+               "\\documentclass[11pt,a4paper]{article}
+\\usepackage{jcappub}
+\\usepackage{float} % Useful for right positioning of figures and tables
+[NO-DEFAULT-PACKAGES]
+[NO-PACKAGES]
+[NO-EXTRA]"
+               ("\\section{%s}" . "\\section*{%s}")
+	       ("\\subsection{%s}" . "\\subsection*{%s}")
+	       ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))

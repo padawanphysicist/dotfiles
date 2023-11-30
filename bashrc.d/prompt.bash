0c08325346b5c36c3ec91a151ac4e18140e534cc
Victor Santos (vct) <victor_santos@fisica.ufc.br>
2023-11-29 22:37:45 -0300

    move files around

---
+++ bashrc.d/prompt.bash
@@ -0,0 +1,66 @@
+if [[ -r "${HOME}/.git-prompt.sh" ]]; then
+    source "${HOME}/.git-prompt.sh"
+    export GIT_PS1_SHOWDIRTYSTATE=1
+    PS1="\n\t \[\033[32m\]\w\[\033[33m\]\$(GIT_PS1_SHOWUNTRACKEDFILES=1 GIT_PS1_SHOWDIRTYSTATE=1 __git_ps1)\[\033[00m\] $ "
+else
+    PS1='\u@\h \w \$ '
+fi
+
+## Set colorful PS1 only on colorful terminals.
+## dircolors --print-database uses its own built-in database
+## instead of using /etc/DIR_COLORS.  Try to use the external file
+## first to take advantage of user additions.
+## We run dircolors directly due to its changes in file syntax and
+## terminal name patching.
+#use_color=false
+#if type -P dircolors >/dev/null ; then
+#	# Enable colors for ls, etc.  Prefer ~/.dir_colors #64489
+#	LS_COLORS=
+#	if [[ -f ~/.dir_colors ]] ; then
+#		eval "$(dircolors -b ~/.dir_colors)"
+#	elif [[ -f /etc/DIR_COLORS ]] ; then
+#		eval "$(dircolors -b /etc/DIR_COLORS)"
+#	else
+#		eval "$(dircolors -b)"
+#	fi
+#	# Note: We always evaluate the LS_COLORS setting even when it's the
+#	# default.  If it isn't set, then `ls` will only colorize by default
+#	# based on file attributes and ignore extensions (even the compiled
+#	# in defaults of dircolors). #583814
+#	if [[ -n ${LS_COLORS:+set} ]] ; then
+#		use_color=true
+#	else
+#		# Delete it if it's empty as it's useless in that case.
+#		unset LS_COLORS
+#	fi
+#else
+#	# Some systems (e.g. BSD & embedded) don't typically come with
+#	# dircolors so we need to hardcode some terminals in here.
+#	case ${TERM} in
+#	[aEkx]term*|rxvt*|gnome*|konsole*|screen|tmux|cons25|*color) use_color=true;;
+#	esac
+#fi
+#
+#if ${use_color} ; then
+#	if [[ ${EUID} == 0 ]] ; then
+#		PS1+='\[\033[01;31m\]\h\[\033[01;34m\] \w \$\[\033[00m\] '
+#	else
+#		if [[ -r "${HOME}/.git-prompt.sh" ]]; then
+#		    source "${HOME}/.git-prompt.sh"
+#		    export GIT_PS1_SHOWDIRTYSTATE=1
+#                    #PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
+#                    #PS1='$(__git_ps1 "(%s) ")\$ '
+#	            PS1="\n\t \[\033[32m\]\w\[\033[33m\]\$(GIT_PS1_SHOWUNTRACKEDFILES=1 GIT_PS1_SHOWDIRTYSTATE=1 __git_ps1)\[\033[00m\] $ "
+#		else
+#		    PS1+='\$ '
+#		fi
+#	fi
+#
+#	alias ls='ls --color=auto'
+#	alias grep='grep --colour=auto'
+#	alias egrep='grep -E --colour=auto'
+#	alias fgrep='grep -F --colour=auto'
+#else
+#	# show root@ when we don't have colors
+#	PS1+='\u@\h \w \$ '
+#fi
